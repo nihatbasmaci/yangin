0b1b5105fc7eb15fb27964b0f299e399d2ad2a66
omfaer <omer.faruk.er.93@gamil.com>
2014-03-10 22:04:29 +0200

    İlk controller ve views oluşturuldu. Dil testi için bir yml dosyası oluşturuldu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Yangin::Application.routes.draw do
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
